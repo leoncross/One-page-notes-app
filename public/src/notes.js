c28c1c2801fc3dd81f8ad0c4c6469210aa846c68
leoncross <leon@lscross.co.uk>
2018-12-11 15:42:37 +0000

    added colours to notes.js and trimmed length

--- public/src/notes.js
+++ public/src/notes.js
@@ -1,8 +1,13 @@
 window.onload=function(){
 
+
+  var textColours = ['red', 'blue', 'yellow', 'green', 'purple', 'orange', 'pink', 'black', 'navy' ]
+
   document.getElementById("submit").addEventListener("click", function(){
-      var note = document.getElementById("text").value
-      var html_to_insert = "<p>"+ note +"</p>";
-      document.getElementById('notes').innerHTML += html_to_insert;
+    var textColoursRand = textColours[Math.floor(Math.random() * textColours.length)];
+    var noteShort = document.getElementById("text").value.substring(0,20)
+    // var noteFull = document.getElementById("text").value
+    var html_to_insert = "<p style=color:" + textColoursRand + ">"+ noteShort +"</p>";
+    document.getElementById('notes').innerHTML += html_to_insert;
   });
 }
