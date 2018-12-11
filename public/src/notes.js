2ea74479e230efb539a48e1c05f82544fb8544ef
leoncross <leon@lscross.co.uk>
2018-12-11 15:55:12 +0000

    refactored notes.js

--- public/src/notes.js
+++ public/src/notes.js
@@ -1,13 +1,17 @@
 window.onload=function(){
 
 
-  var textColours = ['red', 'blue', 'yellow', 'green', 'purple', 'orange', 'pink', 'black', 'navy' ]
-
   document.getElementById("submit").addEventListener("click", function(){
-    var textColoursRand = textColours[Math.floor(Math.random() * textColours.length)];
-    var noteShort = document.getElementById("text").value.substring(0,20)
-    // var noteFull = document.getElementById("text").value
-    var html_to_insert = "<p style=color:" + textColoursRand + ">"+ noteShort +"</p>";
+    var html_to_insert = "<p style=color:" + randColor() + ">"+ shortNote() +"</p>";
     document.getElementById('notes').innerHTML += html_to_insert;
   });
+
+  function randColor() {
+    var colors = ['red', 'blue', 'yellow', 'green' ]
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  function shortNote() {
+    return document.getElementById("text").value.substring(0,20)
+  }
 }
